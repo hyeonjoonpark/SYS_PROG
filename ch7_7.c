ed20c99d0e060b115b1eb01aa81ba5d54a91b9f6
hyeonjoonpark <pjjoon1379@gmail.com>
2023-10-06 11:24:42 +0900

    SYSPROG

---
+++ ch7_7.c
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h> 
+
+int main(){
+    pid_t pid;
+    
+    switch (pid =fork()){
+        case -1: /*fork failed*/
+            perror("fork");
+            exit(1);
+        case 0: /*child process*/
+            printf("--> Child process");
+            if(execlp("ls","ls","-a",(char *)NULL) == -1){
+                perror("execlp");
+                exit(1);
+            }
+            exit(0);
+            break;
+        default : /*parent process*/
+            printf("--> Parent process - My PID:%d\n",(int)getpid());
+            break;
+    }
+}
